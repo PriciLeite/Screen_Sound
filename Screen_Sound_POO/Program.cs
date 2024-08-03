724c1965c59fee4e8dc1aed44f0369f8df40c157
Priscila Leite <pricila_leite@hotmail.com>
2024-08-03 11:30:58 -0300

    Exemplo de uso de expressão Lambda para método

--- Screen_Sound_POO/Program.cs
+++ Screen_Sound_POO/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+
+    static public int Somar(int a, int b) => a + b;
+
+    private static void Main(string[] args)
+    {
+        var resultado1 = Somar(100, 100);
+        System.Console.WriteLine($"Soma: {resultado1}");
+    }
+}
