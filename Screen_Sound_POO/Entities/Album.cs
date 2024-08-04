96a4550da3d71c83b85a96c0477b393657fdb74a
Priscila Leite <pricila_leite@hotmail.com>
2024-08-04 18:48:26 -0300

    Implementando a Duração do Álbum com base nas músicas existentes na lista

--- Screen_Sound_POO/Entities/Album.cs
+++ Screen_Sound_POO/Entities/Album.cs
@@ -1,15 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 
 namespace ScreenSound.ScreenSoundPOO;
 
 class Album
 {
+    private string? nome;
+    public int DuracaoDoAlbum => musicas.Sum(musica => musica.Duracao);
     //Músicas pertencem ao Albúm:
     private List<Musica> musicas = new List<Musica>();
-    private string? nome;
-    //private int duracaoTotal;
+
 
     public string? Nome
     {
@@ -28,11 +30,13 @@ class Album
         }
     }
 
+
     // Para adiconar:
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
     }
+
 
     // Para Exibir: config. em Músicas.
     public void ExibirMusicasAlbum()
@@ -45,7 +49,8 @@ class Album
 
         System.Console.WriteLine($"\n***Músicas do Álbum {nome}***");
         musicas?.ForEach(musica => System.Console.WriteLine(musica));
-    }
 
+        System.Console.WriteLine($"\nPara ouvir este álbum você precisa de: {DuracaoDoAlbum} min");
+    }
 
 }
